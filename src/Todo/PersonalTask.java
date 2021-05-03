601c7d6aa16647f4d71e18c9663e95b896e79887
KarthikB-dev <saberlight35@gmail.com>
2021-05-02 20:14:19 -0700

    Organized files in todo, notes, and timer directories

---
+++ src/Todo/PersonalTask.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class personalTask extends Task
+{
+    //could be for personal fitness, overall health,
+    //personal project, etc.
+    //exercise
+    //diet
+    //personal project
+    //skill to learn(with a deadline, perhaps java.util.date)
+}
---
+++ src/Todo/Task.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Task {
+	//tasks should have subtasks
+    //for example, a task of learning react
+    //js could have a subtask of learning syntax
+    //building a simple webapp, etc.
+    private ArrayList<Task> subtasks;
+    //tasks can have a specific due date and time 
+    //when they are due, they could be due at the end of 
+    //the day, etc.
+    private String description;
+}
