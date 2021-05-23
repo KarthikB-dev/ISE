9118cca8562e146c1b1fec2f9ea7421dfff6a1ac
KarthikB-dev <saberlight35@gmail.com>
2021-05-23 12:16:43 -0700

    Main.java modified to allow for noteIndex to be displayed with the note

--- src/Notes/Main.java
+++ src/Notes/Main.java
@@ -10,6 +10,7 @@ public class Main
 {
 	public static void main(String[] args) 
 	{
+		/*
 		Note cs = new Note("Computer Science");
 		Note netwk = new Note("Networking", 
 			"Various protocols and connections between computers");
@@ -21,26 +22,40 @@ public class Main
 			"Parts of a computer, like the CPU, motherboard, GPU, peripherals, etc.");
 		cs.addNote(hardware);
 		cs.printNotes(0);
+		*/
 		//each time a new note is added, it is added to a TreeMap
 		int noteIndex = 1;
 		TreeMap<Integer, Note> treeNote = new TreeMap<Integer, Note>();
 		Scanner cin = new Scanner(System.in);
+		// System.out.println("Enter nn for a new note");
+		// System.out.println("Enter 'exit' if you wish to exit");
 		while (true) {
 			//if the user wants to enter a new note
-			System.out.println("Enter nn for a new note");
-			System.out.println("Enter 'exit' if you wish to exit");
-			String userInput = cin.next();
+			
+			String userInput = "";
+			while (userInput.equals("")) {
+				System.out.println("Enter nn for a new note");
+				System.out.println("Enter 'exit' if you wish to exit");
+				userInput = cin.next();
+			}
 			if (userInput.equals("nn")) {
+				String section = "";
 				System.out.println("what is the section name?");
-				String section = cin.next();
+				while (section.equals(""))
+				{
+					section = cin.nextLine();
+				}
 				System.out.println("what are the contents? enter ~ if there are none");
-				String contents = cin.next();
+				String contents = "";
+				while (contents.equals("")) {
+					contents = cin.nextLine();
+				}
 				Note newNote;
 				if (contents.equals("~")) {
-					newNote = new Note(section);
+					newNote = new Note(section, noteIndex);
 				}
 				else {
-					newNote = new Note(section, contents);
+					newNote = new Note(section, contents, noteIndex);
 				}
 				if (treeNote.size() == 0) {
 					treeNote.put(1, newNote);
@@ -59,16 +74,29 @@ public class Main
 					//as a key for the treeNote
 					//map, then add this note to
 					//the corresponding note
-					System.out.println("Which note should it be added to?");
-					treeNote.get(cin.nextInt()).addNote(newNote);
+					int index = -1;
+					System.out.println("What note should it be added to?");
+					while (index == -1)
+					{
+						index = cin.nextInt();
+					}
+					Integer objIndex = index;
+					treeNote.get(objIndex).addNote(newNote);
 					treeNote.put(noteIndex, newNote);
 				}
 				noteIndex++;
 			}
+			treeNote.get(1).printNotes(0);
 			if (userInput.equals("exit"))
 			{
 				break;
 			}
+		}
+		if (treeNote.size() >= 1) {
+			treeNote.get(1).printNotes(0);
+		}
+		else {
+			System.out.println("None");
 		}
 		cin.close();
 	}
